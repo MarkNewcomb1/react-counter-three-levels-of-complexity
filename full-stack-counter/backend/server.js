753643eef379a54ff0320209ba6e3241e413074d
MarkNewcomb1 <wlmmn@hotmail.com>
2020-10-06 09:44:18 -0600

    nodemon and concurrently

--- full-stack-counter/backend/server.js
+++ full-stack-counter/backend/server.js
@@ -3,7 +3,7 @@ const counterData = require('./data/counterData')
 const app = express()
 
 app.get('/', (req, res) => {
-    res.send('API is running...')
+    res.send('API is running....')
 })
 
 app.get('/api/counter', (req, res) => {
