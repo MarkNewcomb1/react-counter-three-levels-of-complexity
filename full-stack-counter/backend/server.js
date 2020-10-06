d40fcd91d5b501df06e12fc722ad5da9a667db60
MarkNewcomb1 <wlmmn@hotmail.com>
2020-10-06 10:39:36 -0600

    connected to mongodb, added colors package

--- full-stack-counter/backend/server.js
+++ full-stack-counter/backend/server.js
@@ -1,8 +1,12 @@
 import express from 'express'
 import dotenv from 'dotenv'
+import colors from 'colors'
+import connectDB from './config/db.js'
 import counterData from './data/counterData.js'
 
 dotenv.config();
+
+connectDB()
 const app = express()
 
 app.get('/', (req, res) => {
@@ -15,4 +19,4 @@ app.get('/api/counter', (req, res) => {
 
 const PORT = process.env.PORT || 5000
 
-app.listen(PORT, console.log(`Server running in ${process.env.NODE_ENV} mode on port ${PORT}`))
+app.listen(PORT, console.log(`Server running in ${process.env.NODE_ENV} mode on port ${PORT}`.blue.bold))
