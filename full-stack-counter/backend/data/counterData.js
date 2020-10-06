88d8578b11e83b7118161942685c6f5743945132
MarkNewcomb1 <wlmmn@hotmail.com>
2020-10-06 08:51:55 -0600

    basic api, not hooked to react yet

---
+++ full-stack-counter/backend/data/counterData.js
@@ -0,0 +1,8 @@
+const counterData = [
+    {
+        _id: 1,
+        counter: 0
+    }
+]
+
+module.exports = counterData
---
+++ full-stack-counter/backend/server.js
@@ -0,0 +1,13 @@
+const express = require('express')
+const counterData = require('./data/counterData')
+const app = express()
+
+app.get('/', (req, res) => {
+    res.send('API is running...')
+})
+
+app.get('/api/counter', (req, res) => {
+    res.json(counterData)
+})
+
+app.listen(5000, console.log('Server running on port 5000'))
