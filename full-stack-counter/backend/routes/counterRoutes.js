a0cd618d48875f56d1f04e527c1503e7ba5acf99
MarkNewcomb1 <wlmmn@hotmail.com>
2020-10-14 09:07:43 -0600

    increment api route works

--- full-stack-counter/backend/routes/counterRoutes.js
+++ full-stack-counter/backend/routes/counterRoutes.js
@@ -3,14 +3,27 @@ import asyncHandler from 'express-async-handler'
 const router = express.Router()
 import Counter from '../models/CounterModel.js'
 
+// example counter result:
+// [
+//     {
+//         "_id": "5f7cc277431d551d6e4fdb64",
+//         "counter": 9,
+//         "__v": 0
+//     }
+// ]
+
 // getting the counter
 router.get('/', asyncHandler(async (req, res) => {
     const counterResults = await Counter.find({})
     res.json(counterResults[0].counter)
 }))
 
-// router.put('/', (req, res) => {
-//     res.json(counterData)
-// })
+router.put('/increment', asyncHandler(async (req, res) => {
+    const conditions = { '_id': '5f7cc277431d551d6e4fdb64' },
+    update = { $inc: {counter: 1} };
+    const updateAction = await Counter.update(conditions, update);
+    const counterResults = await Counter.find({})
+    res.json(counterResults[0].counter)
+}))
 
 export default router
